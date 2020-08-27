6529bec789a86b72cedb2c86a8313f4495060136
shub22mishra <47281314+shub22mishra@users.noreply.github.com>
2020-08-27 22:56:35 +0530

    Updates

---
+++ precache-manifest.144a57819ff708aa8d720c35fa6d3470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9d458645fffd7273c001420d681315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5d4ee7e9909f5b427f",
+    "url": "/static/css/main.0c854551.chunk.css"
+  },
+  {
+    "revision": "2676008d7a4571f50207",
+    "url": "/static/js/2.61034361.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.61034361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5d4ee7e9909f5b427f",
+    "url": "/static/js/main.db344737.chunk.js"
+  },
+  {
+    "revision": "ca85c3989b045bc4e3be",
+    "url": "/static/js/runtime-main.6c893ce9.js"
+  }
+]);
