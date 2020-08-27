875082293ea96a6eb1ad01139d30f01e2081bfe6
shub22mishra <47281314+shub22mishra@users.noreply.github.com>
2020-08-27 22:33:16 +0530

    my first commit reactapplication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import wordsToNumbers from "words-to-numbers";
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
+const alanKey =
+  "fa2e1b60b3b2a20ac70dfeb5b5b112f62e956eca572e1d8b807a3e2338fdd0dc/stage";
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
 
-function App() {
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.infoContainer}>
+      <img
+        src="https://alan.app/voice/images/previews/preview.jpg"
+        className={classes.alanLogo}
+        alt="alan logo"
+      />
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
-
+};
 export default App;
